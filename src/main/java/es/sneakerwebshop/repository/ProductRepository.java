24918d4ea460f9982b3e74f627283ecaea1697e2
EmSley77 <emanuelsleyman@gmail.com>
2024-06-11 19:03:59 +0200

    commenting on what methods are needed for basket Service

--- src/main/java/es/sneakerwebshop/repository/ProductRepository.java
+++ src/main/java/es/sneakerwebshop/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import es.sneakerwebshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Product findProductByProductId(int productId);
 }
--- src/main/java/es/sneakerwebshop/service/BasketService.java
+++ src/main/java/es/sneakerwebshop/service/BasketService.java
@@ -5,6 +5,7 @@ package es.sneakerwebshop.service;
 *this service contains all the important methods belonging to basket
 */
 import es.sneakerwebshop.entity.Product;
+import es.sneakerwebshop.repository.ProductRepository;
 import lombok.Getter;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +15,38 @@ import java.util.List;
 @Service
 public class BasketService {
 
+
+    private ProductRepository productRepository;
+
+    public BasketService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Getter
     private List<Product> basket = new ArrayList<>();
 
 
+    //add product to basket...
+    public void addProduct(int productId) {
+        Product product = productRepository.findProductByProductId(productId);
+        if (product == null) {
+            return;
+        }
+
+        if (product.getStock() >= 1) {
+            basket.add(product);
+        }
+    }
+
+
+    //remove product from basket....
+
+
+
+    //decrease product amount, if <1 delete from basket...
+
+
+    //increase product amount, if stock is empty do not continue to increase amount....
 
 
 }
