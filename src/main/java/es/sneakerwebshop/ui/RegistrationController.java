d99eeed26492baa78ee70669b4c2991c2cf5ec35
EmSley77 <emanuelsleyman@gmail.com>
2024-06-11 19:57:23 +0200

    just made it more secure when registering

--- src/main/java/es/sneakerwebshop/ui/RegistrationController.java
+++ src/main/java/es/sneakerwebshop/ui/RegistrationController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegistrationController {
@@ -21,11 +22,18 @@ public class RegistrationController {
     }
 
     @PostMapping("sneaker-register-user")
-    public String registerUser(String name, String lastname, String email, String password, int telephoneNumber, String address, Model model) {
+    public String registerUser(@RequestParam String name,
+                               @RequestParam String lastname,
+                               @RequestParam String email,
+                               @RequestParam String password,
+                               @RequestParam int telephoneNumber,
+                               @RequestParam String address,
+                               @RequestParam String repeatPassword,
+                               @RequestParam String repeatEmail,
+                               Model model) {
 
         String result = userService.createAccount(name, lastname, email, password, telephoneNumber, address);
-        if (result.equals("Your account has been successfully created, enjoy shopping " + name)) {
-
+        if (result.equals("Your account has been successfully created, enjoy shopping " + name) && repeatEmail.equals(email) && repeatPassword.equals(password)) {
             return "sneaker_loginpage";
         }
         else  {
