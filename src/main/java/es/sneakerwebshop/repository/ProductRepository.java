98e6eab65935474807025f413261518ca8b835dc
EmSley77 <emanuelsleyman@gmail.com>
2024-06-12 11:23:20 +0200

    creating search service

--- src/main/java/es/sneakerwebshop/repository/ProductRepository.java
+++ src/main/java/es/sneakerwebshop/repository/ProductRepository.java
@@ -3,6 +3,10 @@ package es.sneakerwebshop.repository;
 import es.sneakerwebshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Product findProductByProductId(int productId);
+
+    List<Product> findByNameContainingOrCategoryContainingOrBrandContaining(String search, String search1, String search2);
 }
---
+++ src/main/java/es/sneakerwebshop/service/SearchService.java
@@ -0,0 +1,37 @@
+package es.sneakerwebshop.service;
+/*
+ * Emanuel sleyman
+ * 2024-06-12
+ * Service for everyhitng that has with searching to do
+ */
+import es.sneakerwebshop.entity.Product;
+import es.sneakerwebshop.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchService {
+
+
+    private ProductRepository productRepository;
+
+    public SearchService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //search for product
+    public List<Product> searchForProducts(String search) {
+        return productRepository.findByNameContainingOrCategoryContainingOrBrandContaining(search, search, search);
+    }
+
+    // get all products
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+
+
+
+
+}
