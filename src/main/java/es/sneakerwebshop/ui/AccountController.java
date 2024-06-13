12d4d360ce0bda42b0c4eab73041be9c37d73fb7
EmSley77 <emanuelsleyman@gmail.com>
2024-06-13 20:46:26 +0200

    Starting on account controller and page

---
+++ src/main/java/es/sneakerwebshop/ui/AccountController.java
@@ -0,0 +1,11 @@
+package es.sneakerwebshop.ui;
+/*
+* Emanuel sleyman
+* 2024-06-13
+* controller class responisble for user account
+*/
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AccountController {
+}
