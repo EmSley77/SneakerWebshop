ddfb5029efa83ba31546283b2d96f4cebed5109b
EmSley77 <emanuelsleyman@gmail.com>
2024-06-14 21:00:06 +0200

    creating order service and commenting

---
+++ src/main/java/es/sneakerwebshop/service/OrderService.java
@@ -0,0 +1,11 @@
+package es.sneakerwebshop.service;
+/*
+ * Emanuel sleyman
+ * 2024-06-11
+ * this service is for Order methods, ordering, viewing orders, and other
+*/
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+}
