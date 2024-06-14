b10c4aac97859e4ad290352dac1b384a62f6fcf7
EmSley77 <emanuelsleyman@gmail.com>
2024-06-14 21:01:57 +0200

    commenting todos

--- src/main/java/es/sneakerwebshop/service/OrderService.java
+++ src/main/java/es/sneakerwebshop/service/OrderService.java
@@ -4,8 +4,34 @@ package es.sneakerwebshop.service;
  * 2024-06-11
  * this service is for Order methods, ordering, viewing orders, and other
 */
+import es.sneakerwebshop.entity.Order;
+import es.sneakerwebshop.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    // Make order
+
+
+
+    // view order , admin & user
+
+
+    // get order details, with order id, Admin and user
+
+
+    // get all orders, Admin
+
+
+
+
+
+
 }
