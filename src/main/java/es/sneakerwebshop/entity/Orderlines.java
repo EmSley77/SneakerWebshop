a6d3ffba3064366e52c3a1cb905c6ea910142c88
EmSley77 <emanuelsleyman@gmail.com>
2024-06-15 12:32:06 +0200

    refining orderline entity

--- src/main/java/es/sneakerwebshop/entity/Orderlines.java
+++ src/main/java/es/sneakerwebshop/entity/Orderlines.java
@@ -1,10 +1,13 @@
 package es.sneakerwebshop.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 import java.util.Objects;
-
+@Data
+@NoArgsConstructor
 @Entity
 public class Orderlines {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,67 +27,13 @@ public class Orderlines {
     @Column(name = "total_cost", nullable = false)
     private int totalCost;
     @Basic
+    @Column(name = "product_image", nullable = false)
+    private byte[] productImage;
+    @Basic
+    @Column(name = "product_name", nullable = false)
+    private byte[] productName;
+    @Basic
     @Column(name = "order_time", nullable = false)
     private Timestamp orderTime;
 
-    public int getOrderlineId() {
-        return orderlineId;
-    }
-
-    public void setOrderlineId(int orderlineId) {
-        this.orderlineId = orderlineId;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public int getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(int totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public Timestamp getOrderTime() {
-        return orderTime;
-    }
-
-    public void setOrderTime(Timestamp orderTime) {
-        this.orderTime = orderTime;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Orderlines that = (Orderlines) o;
-        return orderlineId == that.orderlineId && orderId == that.orderId && productId == that.productId && amount == that.amount && totalCost == that.totalCost && Objects.equals(orderTime, that.orderTime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(orderlineId, orderId, productId, amount, totalCost, orderTime);
-    }
 }
